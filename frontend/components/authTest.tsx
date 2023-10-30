f162e737ebc77935578babf28af39e4ff2c78b27
Thomas Zhou <50957137+thomaszhou01@users.noreply.github.com>
2023-10-30 19:10:13 -0400

    dynamic route

--- frontend/components/authTest.tsx
+++ frontend/components/authTest.tsx
@@ -4,9 +4,12 @@ import { Amplify, Auth, Hub } from 'aws-amplify';
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 import awsConfig from '../app/aws-exports';
 
-Amplify.configure(awsConfig);
-
 function AuthTest() {
+	useEffect(() => {
+		awsConfig.oauth.redirectSignIn = `${window.location.origin}/`;
+		awsConfig.oauth.redirectSignOut = `${window.location.origin}/`;
+		Amplify.configure(awsConfig);
+	}, []);
 	return (
 		<div className="App">
 			<button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
