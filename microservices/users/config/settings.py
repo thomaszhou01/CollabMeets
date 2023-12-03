eb65351f51e86fab9686541b3175792259fd598d
Thomas Zhou <50957137+thomaszhou01@users.noreply.github.com>
2023-12-02 19:28:53 -0500

    update users service

--- microservices/users/config/settings.py
+++ microservices/users/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "users.apps.UsersConfig",
 ]
 
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "config.urls"
 
