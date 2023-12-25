599301df561bd4de3473d2192a86f0fca1790507
Thomas Zhou <50957137+thomaszhou01@users.noreply.github.com>
2023-12-25 00:12:48 -0500

    spacing changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,12 +3,12 @@ import Hero from '@/components/mainMenu/hero';
 import { Providers } from './providers';
 
 export default function Home() {
-	return (
-		<Providers>
-			<div className="h-[100vh] max-w-7xl mx-auto">
-				<Header />
-				<Hero />
-			</div>
-		</Providers>
-	);
+    return (
+        <Providers>
+            <div className="h-[100vh] max-w-7xl mx-auto">
+                <Header />
+                <Hero />
+            </div>
+        </Providers>
+    );
 }
