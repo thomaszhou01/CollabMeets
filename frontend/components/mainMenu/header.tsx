1de8e12d2c241556360a0728d78da1d13cf5da9f
Thomas Zhou <50957137+thomaszhou01@users.noreply.github.com>
2023-12-25 00:30:51 -0500

    trigger build

--- frontend/components/mainMenu/header.tsx
+++ frontend/components/mainMenu/header.tsx
@@ -1,13 +1,13 @@
 import UserSignin from '../auth/userSignin';
 
 function Header() {
-	return (
-		<div className="absolute left-0 top-0 w-full">
-			<div className="flex justify-end w-full p-2">
-				<UserSignin />
-			</div>
-		</div>
-	);
+    return (
+        <div className="absolute left-0 top-0 w-full">
+            <div className="flex justify-end w-full p-2">
+                <UserSignin />
+            </div>
+        </div>
+    );
 }
 
 export default Header;
